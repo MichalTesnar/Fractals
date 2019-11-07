9e8f4367c6e8d8a2f890a578f5102f9304d64813
Michal Tesnar <michal.tesnar007@gmail.com>
2019-11-07 08:57:56 +0100

    Square fractal
    Adding fully functional square fracrat

---
+++ constants.py
@@ -0,0 +1,10 @@
+import pygame, sys
+
+BASE_WIDTH = 800
+BASE_HEIGHT = 800
+
+DISPLAY = pygame.display.set_mode((BASE_WIDTH, BASE_HEIGHT), 0, 32)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+
+RECURSION_DEPTH = 5
---
+++ main.py
@@ -0,0 +1,18 @@
+from constants import *
+
+def main():
+    pygame.init()
+
+    DISPLAY.fill(WHITE)
+
+    #import square
+
+    import snowflake
+
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+main()
